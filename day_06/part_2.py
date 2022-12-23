9dc98f87a62b5acc5569423530f7b8add5222d30
Calo <79152594+Objectivitix@users.noreply.github.com>
2022-12-23 18:12:19 -0500

    Add day 6 solution

---
+++ day_06/part_2.py
@@ -0,0 +1,18 @@
+SLIDING_WINDOW_SPAN = 14
+
+with open("./input.txt") as file:
+    input = file.read()
+
+marker = None
+
+length = len(input)
+
+for start in range(length):
+    end = start + SLIDING_WINDOW_SPAN
+
+    window = input[start:end]
+    if len(set(window)) == SLIDING_WINDOW_SPAN:
+        marker = end
+        break
+
+print(marker)
